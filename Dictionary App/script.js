25c86a16d5e29b04f1be5941be9c54a39e724113
faizanlkhan <155158189+faizanlkhan@users.noreply.github.com>
2024-08-12 05:13:51 +0530

    Update script.js

--- Dictionary App/script.js
+++ Dictionary App/script.js
@@ -1,40 +1,49 @@
 // apiUrl = https://api.dictionaryapi.dev/api/v2/entries/en/
 
 const apiUrl = "https://api.dictionaryapi.dev/api/v2/entries/en/"
-const result = document.querySelector(".result")
-const sound = document.getElementById("sound")
 const button = document.getElementById("search-btn")
-const searchbox = document.querySelector(".search-box")
+const inpword = document.getElementById("input-word")
+const result = document.getElementById("result")
+const sound = document.getElementById("sound")
 
-button.addEventListener("click", () => {
-    let inpword = document.getElementById("input-word").value
-    const response = fetch(`${apiUrl}${inpword}`).then(response => response.json()).then(data => {
+
+async function getMeaning(word) {
+    const response = await fetch(`${apiUrl}${word}`)
+    const value = await response.json().then(data => {
         console.log(data)
         sound.src = `${data[0].phonetics[0].audio}`
         result.innerHTML = `
-                <div class="word">
-                    <h3>${inpword}</h3>
-                    <button onclick="playSound()">
-                        <i class="fa-solid fa-volume-high"></i>
-                    </button>
-                </div>
-                <div class="details">
-                    <p>${data[0].meanings[0].partOfSpeech}</p>
-                    <p>${data[0].phonetics[0].text}</p>
-                </div>
-                <p class="word-meaning">
-                ${data[0].meanings[0].definitions[0].definition}
-                </p>          
-                <p class="word-example">${data[0].meanings[0].definitions[0].example ? data[0].meanings[0].definitions[0].example : ""}<p>`
-        // Getmeaning(inpword)
-    }).catch((error)=>{
-        result.innerHTML= `<p class = "error"> *Couldn't find the word</p>`
-        console.error(error)
-    })
-})
+            <div class="word">
+            <h3>${word}</h3>
+            <button onclick="playSound()">
+                            <i class="fa-solid fa-volume-high"></i>
+                            </button>
+                            </div>
+                            <div class="details">
+                            <p>${data[0].meanings[0].partOfSpeech}</p>
+                            <p>${data[0].phonetics[0].text}</p>
+                            </div>
+                            <p class="word-meaning">
+                            ${data[0].meanings[0].definitions[0].definition}
+                            </p>          
+                            <p class="word-example">${data[0].meanings[0].definitions[0].example ? data[0].meanings[0].definitions[0].example : ""}<p>`
 
-function playSound(){
-    sound.play();
+
+    }).catch((error) => {
+        if (inpword.value.toLowerCase() === "motu" || "motuu" || "motuuu") {
+            result.innerHTML = `<p class = "error">Motu means you ><</p>`
+            console.error(error)
+        } else {
+            result.innerHTML = `<p class = "error">*Could'nt find the word "${word}"</p>`
+            console.error(error)
+        }
+    })
 }
 
-// Hey there if you are reading this, I hope you have nice day
+button.addEventListener("click", () => {
+    getMeaning(inpword.value)
+})
+
+function playSound() {
+    sound.play()
+}
